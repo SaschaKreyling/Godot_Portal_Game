a2d35260987cfce6acf5e218a6f7249ae8d03ac4
Haertrich <167681180+Haertrich@users.noreply.github.com>
2025-03-04 12:04:51 +0100

    Item grab
    Box added to global holdable group
    ray cast 3d added to player

---
+++ scripts/ray_cast_3d.gd
@@ -0,0 +1,20 @@
+extends RayCast3D
+
+var object = null
+var key = KEY_E
+@onready var point = $"../Camera3D/Hold"
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if Input.is_key_pressed(key):
+		if object == null:
+			var collider = get_collider()
+			if collider != null:
+				if collider.is_in_group("Holdables"):
+					object = collider
+		
+		if object != null:
+			object.position = point.global_position
+	else:
+		object = null
