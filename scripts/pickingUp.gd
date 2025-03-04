e8e70252971f5a8149f58b4a25f1324b1ac1c156
Jan Härtrich <janhaertrich@gmail.com>
2025-03-04 21:04:54 +0100

    added throw function
    einmal ging der wurf schräg nach unten aber danach net mehr idk was da los war

--- scripts/pickingUp.gd
+++ scripts/pickingUp.gd
@@ -1,6 +1,7 @@
 extends RayCast3D
 
 @onready var holdingPoint: Node3D = $"../Hold"
+@onready var camera: Camera3D = $".."
 
 var pickedUp : bool = false
 var collider : CollisionObject3D
@@ -17,3 +18,19 @@ func _process(_delta: float) -> void:
 	
 	if pickedUp and collider != null:
 		collider.global_position = holdingPoint.global_position
+	
+	if Input.is_action_just_pressed("throw"):
+		throw_object()
+
+
+func throw_object():
+	if collider == null:
+		return
+	
+	var throw_direction = camera.global_transform.basis.z.normalized()
+	var throw_strength = -18.0
+	var upDirection = 5
+	
+	pickedUp = false
+	collider.apply_impulse(throw_direction * throw_strength + Vector3(0, upDirection, 0))
+	collider = null
