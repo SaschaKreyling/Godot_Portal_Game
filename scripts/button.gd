b28a32a456f7b9dc4f42d62aa6adccb9cadd37a1
EnvySloth <sgtcurrys16@gmail.com>
2025-03-07 00:50:22 +0100

    working gum/glue

--- scripts/button.gd
+++ scripts/button.gd
@@ -2,47 +2,103 @@ extends StaticBody3D
 class_name FloorButton
 
 @export_color_no_alpha var linkColor : Color
+@export var unglue : bool = false
 
 @onready var active_collider: CollisionShape3D = $ActiveCollider
 @onready var deactive_collider: CollisionShape3D = $DeactiveCollider
+
 @onready var button_deactive_mesh: MeshInstance3D = $ButtonDeactive
 @onready var button_active_mesh: MeshInstance3D = $ButtonActive
+@onready var button_glued_mesh: MeshInstance3D = $ButtonGlued
+
 @onready var identfier : Identifier = $Identifier
 
 var currentActivators : Array = []
 var activated: bool = false
+
+var glued: bool = false
+var currentGumBall: GumBall
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	identfier.updateColor(linkColor)
 	setDeactivated()
 
-func setActivated() -> void:
+func _process(delta: float) -> void:
+	if unglue:
+		unglue = false
+		setUnglued()
+
+func setGlued(gumBall : GumBall) -> bool:
+	if(not activated or glued):
+		return false
+	
+	print("setting glued")
+		
+	glued = true
 	activated = true
-	active_collider.set_deferred("disable",false) 
+	currentGumBall = gumBall
+	
+	active_collider.set_deferred("disable", false)
+	button_glued_mesh.visible = true
+	
+	deactive_collider.set_deferred("disable", true) 
 	button_deactive_mesh.visible = false
 	
-	deactive_collider.set_deferred("disable",true) 
+	button_active_mesh.visible = false
+	
+	return true
+
+func setUnglued() -> void:
+	if glued:
+		glued = false
+		currentGumBall.reset()
+		currentGumBall = null
+		updateState()
+
+func setActivated() -> void:
+	glued = false
+	activated = true
+
+	print("setting active")
+
+	active_collider.set_deferred("disable", false) 
 	button_active_mesh.visible = true
+	
+	deactive_collider.set_deferred("disable", true) 
+	button_deactive_mesh.visible = false
+	
+	button_glued_mesh.visible = false
 
 func setDeactivated() -> void:
+	glued = false
 	activated = false
-	active_collider.set_deferred("disable",true)
+	
+	print("setting deactive")
+	
+	deactive_collider.set_deferred("disable", false)
 	button_deactive_mesh.visible = true
 	
-	deactive_collider.set_deferred("disable",false)
+	active_collider.set_deferred("disable", true)
 	button_active_mesh.visible = false
+	
+	button_glued_mesh.visible = false
+
+func updateState():
+	activated = not currentActivators.is_empty() or glued
+	print("UpdateState: " + str(activated) + " Glued? " + str(glued))
+	if not glued:
+		if activated:
+			setActivated()
+		else:
+			setDeactivated()
 
 func _on_activation_area_body_entered(body: Node3D) -> void:
 	if(body.is_in_group("presser")):
 		currentActivators.append(body)
-	activated = not currentActivators.is_empty()
-	if activated:
-		setActivated()
+	updateState()
 
 func _on_activation_area_body_exited(body: Node3D) -> void:
 	if(body.is_in_group("presser")):
 		currentActivators.erase(body)
-	activated = not currentActivators.is_empty()
-	if not activated:
-		setDeactivated()
+	updateState()
--- scripts/gum_ball.gd
+++ scripts/gum_ball.gd
@@ -4,28 +4,36 @@ class_name GumBall
 @onready var interactCollider: CollisionShape3D = $InteractCollider
 @onready var hitZoneCollider: CollisionShape3D = $HitZone/HitZoneCollider
 @onready var gumBallMesh: MeshInstance3D = $GumBallMesh
+@onready var hitZone: Area3D = $HitZone
 
 var startPosition : Vector3
 var inUse : bool = false
-var gluedButton : FloorButton
+var gluedObject
 
 func _ready() -> void:
 	startPosition = global_position
 
 func reset() -> void:
-	gluedButton = null
+	gluedObject = null
 	inUse = false
-	interactCollider.disabled = false
-	hitZoneCollider.disabled = false
+	gumBallMesh.visible = true
+	interactCollider.set_deferred("disabled", false) 
+	hitZoneCollider.set_deferred("disabled", false) 
 	linear_velocity = Vector3(0,0,0)
 	global_position = startPosition
 
-func onButtonHit(button : FloorButton) -> void:
-	inUse = true
-	interactCollider.disabled = true
-	hitZoneCollider.disabled = true
-	gluedButton = button
-	pass
+func onGlueableObjectHit(gluableObject) -> void:
+	if gluableObject.setGlued(self):
+		inUse = true
+		interactCollider.set_deferred("disabled", true) 
+		hitZoneCollider.set_deferred("disabled", true) 
+		gluedObject = gluableObject
+		gumBallMesh.visible = false
+	else:
+		reset()
 
 func _on_hit_zone_body_entered(body: Node3D) -> void:
-	reset()
+	if body.is_in_group("gluable"):
+		onGlueableObjectHit(body)
+	else:
+		reset()
