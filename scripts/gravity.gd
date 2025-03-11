6e4257a7132785c0de6d0168361d0b02f26c6ed4
EnvySloth <sgtcurrys16@gmail.com>
2025-03-11 22:08:15 +0100

    gravity chenanigans

--- scripts/gravity.gd
+++ scripts/gravity.gd
@@ -1,11 +1,6 @@
 extends Node3D
 
 @onready var area: Area3D = $"."
-signal gravity_turned(gravity: int)
-
-var gravity_normal: bool = true
-var up = Vector3(0, 1, 0)
-var down = Vector3(0, -1, 0)
 
 @export var upButton : StandingButton
 @export var downButton : StandingButton
@@ -14,20 +9,11 @@ func _ready() -> void:
 	upButton.standing_button_pushed.connect(setUp)
 	downButton.standing_button_pushed.connect(setDown)
 
-func gravity(direction) -> void:
+func setGravity(direction) -> void:
 	area.set_gravity_direction(direction)
-	
-	var target_rotation
-	if direction == up:
-		gravity_normal = false
-		target_rotation = 180
-		emit_signal("gravity_turned", -1)
-	else:
-		gravity_normal = true
-		target_rotation = 0
-		emit_signal("gravity_turned", 1)
 		
 func setUp():
-	gravity(up)
+	setGravity(Vector3.UP)
+
 func setDown():
-	gravity(down)
+	setGravity(Vector3.DOWN)
--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -9,52 +9,75 @@ class_name Player extends CharacterBody3D
 @onready var ui: Node = $Camera3D/UI
 
 var paused = false
-var gravity_normal = 1
-var gravity_switched = false
+var turning = false
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
 func _input(event):
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		rotate(Vector3.UP,-event.relative.x * 0.001 * gravity_normal)
+		rotate(global_basis.y, -event.relative.x * 0.001)
 		camera.rotate_x(-event.relative.y * 0.001)
 		camera.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
 
 func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
+var gravityTurned = false
 func _process(delta) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	if updateUp():
+		gravityTurned = true
+
+	turnToGravity()
 	
-	if gravity_normal == -1 and not gravity_switched:
-		velocity += get_gravity() * delta
-		gravity_switched = true
-		up_direction = Vector3(0, -1, 0)
+	var localVelocity = global_basis.inverse() * velocity
 	
-	if gravity_normal == 1:
-		gravity_switched = false
-		up_direction = Vector3(0, 1, 0)
-	
-	if Input.is_action_just_pressed("Up") and is_on_floor() :
-		velocity.y = JUMP_VELOCITY * gravity_normal
-		
-	var input_dir := Input.get_vector("Left", "Right", "Forward", "Backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	if !gravityTurned:
+		if Input.is_action_just_pressed("Up") and is_on_floor() :
+			localVelocity.y = JUMP_VELOCITY 
+			
+		var input_dir := Input.get_vector("Left", "Right", "Forward", "Backward")
+		var direction := (Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if direction:
+			localVelocity.x = direction.x * SPEED
+			localVelocity.z = direction.z * SPEED
+		else:
+			localVelocity.x = move_toward(velocity.x, 0, SPEED)
+			localVelocity.z = move_toward(velocity.z, 0, SPEED)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-	
+		localVelocity.x = 0
+		localVelocity.z = 0
+		
+	if not is_on_floor():
+		localVelocity.y += -get_gravity().length() * delta
+	elif gravityTurned and not turning:
+			gravityTurned = false 
+		
 	#Check for pause input
 	if Input.is_action_just_pressed("pause"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		ui.togglePauseMenu()
+		
+	velocity = global_basis * localVelocity
 
-func _on_gravity_gravity_turned(gravity: int) -> void:
-	gravity_normal = gravity
-	var tween = get_tree().create_tween()
-	tween.tween_property(self, "rotation_degrees:z", 180 + gravity_normal * 180, 0.8)
+const rotationSpeed = 0.05
+
+func updateUp() -> bool:
+	var newUp = -get_gravity().normalized()
+	var changed = up_direction != newUp
+	up_direction = newUp
+	return changed
+	
+func turnToGravity() -> void:
+	
+	var targetUp = up_direction
+	var currentUp = global_basis.y
+	
+	var angle : float = min(rotationSpeed, acos(currentUp.dot(up_direction)))
+	if angle != 0:
+		turning = true
+		var ortho = currentUp.cross(targetUp).normalized()
+		var axis = ortho if ortho.length() > 0 else global_basis.z
+		rotate(axis , angle)
+	else:
+		turning = false
