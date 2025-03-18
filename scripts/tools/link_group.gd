ffc759a8dd2bcbb6aa8a3e764573357471b38f4b
EnvySloth <sgtcurrys16@gmail.com>
2025-03-18 13:23:53 +0100

    more cleanup

--- scripts/tools/link_group.gd
+++ scripts/tools/link_group.gd
@@ -1,9 +1,9 @@
 extends Node3D
 
-@export_color_no_alpha var linkColor : Color
+@export_color_no_alpha var link_color : Color
 
 func _ready() -> void:
 	var children = get_children()
 	for child : Object in children:
 		if child.has_method("set_link_color"):
-			child.set_link_color(linkColor)
+			child.set_link_color(link_color)
